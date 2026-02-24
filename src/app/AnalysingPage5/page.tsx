e090ae83a0a5008ae54847194d87022a761972b1
PratGit1606 <prathamgithub16@gmail.com>
2026-02-24 01:36:05 -0700

    V3

--- src/app/AnalysingPage5/page.tsx
+++ src/app/AnalysingPage5/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import AnalysingSidebar from '../components/Sidebar';
-import ProgressSteps from '../components/AnalysingProgressSteps';
+import ProgressSteps from '../components/ProgressSteps';
 import {
     Search,
 } from "lucide-react";
